8cbd0b9e37362273fabca9832efd2d7190a214cf
a.bondarenko <bondarenko.devel@gmail.com>
2026-01-24 16:52:24 +0700

    init commit

---
+++ Pixar Fun/Pixar_FunApp.swift
@@ -0,0 +1,32 @@
+//
+//  Pixar_FunApp.swift
+//  Pixar Fun
+//
+//  Created by Александр Бондаренко on 24.01.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Pixar_FunApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
